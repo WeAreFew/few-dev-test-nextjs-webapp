2d5a11f2cbce7b9afffd32d5bc33ce81c4544703
Wade Austin <wade@few.io>
2023-07-18 17:03:48 -0500

    update styles

--- src/components/organisms/SiteHeader/SiteHeader.tsx
+++ src/components/organisms/SiteHeader/SiteHeader.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./SiteHeader.module.scss";
+import Logo from "@/assets/svgs/main-logo.svg";
 interface Props {}
 
 export default function SiteHeader({}: Props) {
-  return <header className={styles.siteHeader}>SiteHeader</header>;
+  return (
+    <header className={styles.siteHeader}>
+      <Logo />
+      SiteHeader
+    </header>
+  );
 }
