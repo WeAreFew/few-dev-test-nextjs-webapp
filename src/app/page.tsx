3e72cfbf662bc95497bbd198aec8c5df16864b6c
Wade Austin <wade@few.io>
2023-07-18 18:40:59 -0500

    update dev test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
+import Button from "@mui/material/Button";
 import styles from "./page.module.scss";
 import clsx from "clsx";
 export default function Home() {
   return (
     <main className={clsx("section", styles.homePageContent)}>
-      <h1>Few Developer Test</h1>
-      <p>
-        Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
-        vestibulum. Nulla vitae elit libero, a pharetra augue. Donec id elit non
-        mi porta gravida at eget metus.
-      </p>
-      <p>
-        Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus
-        magna, vel scelerisque nisl consectetur et. Etiam porta sem malesuada
-        magna mollis euismod. Vestibulum id ligula porta felis euismod semper.
-        Maecenas faucibus mollis interdum.
-      </p>
+      <div class="container">
+        <h1>Few Developer Test</h1>
+        <p>
+          Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+          vestibulum. Nulla vitae elit libero, a pharetra augue. Donec id elit
+          non mi porta gravida at eget metus.
+        </p>
+        <p>
+          Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus
+          magna, vel scelerisque nisl consectetur et. Etiam porta sem malesuada
+          magna mollis euismod. Vestibulum id ligula porta felis euismod semper.
+          Maecenas faucibus mollis interdum.
+        </p>
+        <Button variant="outlined">Sign Up</Button>
+      </div>
     </main>
   );
 }
