a7b873ea244b1f98f240f2db69f8fac4f348a736
Wade Austin <wade@few.io>
2023-07-18 18:19:45 -0500

    update styles

--- src/components/organisms/SiteFooter/SiteFooter.tsx
+++ src/components/organisms/SiteFooter/SiteFooter.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./SiteFooter.module.scss";
+import Link from "next/link";
 interface Props {}
 
 function SiteFooter({}: Props) {
   return (
     <footer className={styles.siteFooter}>
+      <nav className={styles.legalNav}>
+        <Link href="/privacy-policy">Privacy Policy</Link>
+        <Link href="/terms">Terms and Conditions</Link>
+      </nav>
+
       <p className={styles.copyright}>
-        &copy; {new Date().getFullYear()} Work With Few.
+        &copy; Copyright {new Date().getFullYear()} | Few.
       </p>
     </footer>
   );
