9902d92e9c20be75970ac1303580db4f11c0e1f2
Scott Cameron <mscottcam@gmail.com>
2023-07-20 14:44:15 -0500

    form submission setup, navigates to success route

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
 import clsx from "clsx";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -5,53 +8,69 @@ import TextField from "@mui/material/TextField";
 import styles from "./page.module.scss";
 
 export default function SignUp() {
+  const router = useRouter();
+
+  const onFormSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // save user data
+    router.push("/sign-up/success");
+  };
+
   return (
     <main className="section">
-      <div className={clsx("container", styles.formContainer)}>
+      <div className={clsx("container", styles.signupContainer)}>
         <h1>Sign Up</h1>
         <p>
           Already have an acount? <a>Log In</a>
         </p>
-        <TextField
-          variant="outlined"
-          label="Email"
-          placeholder="Enter your email"
-          required
-          fullWidth
-        />
-        <TextField
-          variant="outlined"
-          label="First Name"
-          placeholder="Enter your first name"
-          required
-          fullWidth
-        />
-        <TextField
-          variant="outlined"
-          label="Last Name"
-          placeholder="Enter your first name"
-          required
-          fullWidth
-        />
-        <TextField
-          variant="outlined"
-          label="Password"
-          type="password"
-          placeholder="Enter your first name"
-          required
-          fullWidth
-        />
-        <TextField
-          variant="outlined"
-          label="Confirm Password"
-          type="password"
-          placeholder="Enter your first name"
-          required
-          fullWidth
-        />
-        <Button variant="contained" color="primary" fullWidth>
-          Continue
-        </Button>
+        <form className={styles.formContainer} onSubmit={onFormSubmit}>
+          <TextField
+            variant="outlined"
+            label="Email"
+            placeholder="Enter your email"
+            required
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            label="First Name"
+            placeholder="Enter your first name"
+            required
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            label="Last Name"
+            placeholder="Enter your first name"
+            required
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            label="Password"
+            type="password"
+            placeholder="Enter your first name"
+            required
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            label="Confirm Password"
+            type="password"
+            placeholder="Enter your first name"
+            required
+            fullWidth
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            type="submit"
+          >
+            Continue
+          </Button>
+        </form>
       </div>
     </main>
   );
