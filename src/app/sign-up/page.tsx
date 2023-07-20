a97fbc877df1e764b8951776e5db182da26c4ba2
Scott Cameron <mscottcam@gmail.com>
2023-07-20 16:42:30 -0500

    adds form validation for each input field

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,19 +1,53 @@
 "use client";
-import { FormEvent } from "react";
 import { useRouter } from "next/navigation";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import clsx from "clsx";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 
 import styles from "./page.module.scss";
 
+const userSchema = z
+  .object({
+    email: z.string().email({ message: "Please enter valid email" }),
+    firstName: z.string().min(1, { message: "First name is required" }),
+    lastName: z.string().min(1, { message: "Last name is required" }),
+    password: z
+      .string()
+      .min(8, {
+        message:
+          "Password must contain at least 8 characters that include at least one uppercase character, 1 number, and one special character.",
+      })
+      .refine(
+        (password) => /(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9])/.test(password),
+        {
+          message:
+            "Password must contain at least 8 characters that include at least one uppercase character, 1 number, and one special character.",
+        }
+      ),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
 export default function SignUp() {
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(userSchema),
+  });
 
-  const onFormSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    // save user data
-    router.push("/sign-up/success");
+  const onFormSubmit: SubmitHandler<FieldValues> = (formData) => {
+    console.log({ formData });
+
+    // router.push("/sign-up/success");
   };
 
   return (
@@ -23,43 +57,66 @@ export default function SignUp() {
         <p>
           Already have an acount? <a>Log In</a>
         </p>
-        <form className={styles.formContainer} onSubmit={onFormSubmit}>
+        <form
+          className={styles.formContainer}
+          onSubmit={handleSubmit(onFormSubmit)}
+        >
           <TextField
+            id="emailField"
             variant="outlined"
             label="Email"
             placeholder="Enter your email"
             required
             fullWidth
+            {...register("email")}
+            helperText={errors.email?.message as string | undefined}
+            error={!!errors.email}
           />
           <TextField
+            id="firstNameField"
             variant="outlined"
             label="First Name"
             placeholder="Enter your first name"
             required
             fullWidth
+            {...register("firstName")}
+            helperText={errors.firstName?.message as string | undefined}
+            error={!!errors.firstName}
           />
           <TextField
+            id="lastNameField"
             variant="outlined"
             label="Last Name"
-            placeholder="Enter your first name"
+            placeholder="Enter your last name"
             required
             fullWidth
+            {...register("lastName")}
+            helperText={errors.lastName?.message as string | undefined}
+            error={!!errors.lastName}
           />
           <TextField
+            id="passwordField"
             variant="outlined"
             label="Password"
             type="password"
-            placeholder="Enter your first name"
+            placeholder="Enter your password"
             required
             fullWidth
+            {...register("password")}
+            helperText={errors.password?.message as string | undefined}
+            error={!!errors.password}
           />
           <TextField
+            id="confirmPasswordField"
             variant="outlined"
             label="Confirm Password"
             type="password"
-            placeholder="Enter your first name"
+            placeholder="Confirm your password"
             required
             fullWidth
+            {...register("confirmPassword")}
+            helperText={errors.confirmPassword?.message as string | undefined}
+            error={!!errors.confirmPassword}
           />
           <Button
             variant="contained"
