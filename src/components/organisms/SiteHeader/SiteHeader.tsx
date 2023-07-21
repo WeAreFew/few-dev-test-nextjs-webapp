cbf988cecf28c5a110843a36217ca56229c570c4
Scott Cameron <mscottcam@gmail.com>
2023-07-21 10:21:43 -0500

    shows log out button in header on user success

--- src/components/organisms/SiteHeader/SiteHeader.tsx
+++ src/components/organisms/SiteHeader/SiteHeader.tsx
@@ -1,21 +1,30 @@
+"use client";
 import React from "react";
 import styles from "./SiteHeader.module.scss";
 import Logo from "@/assets/svgs/main-logo.svg";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Button } from "@mui/material";
 interface Props {}
 
 export default function SiteHeader({}: Props) {
+  const pathname = usePathname();
+  const successScreenVisible = pathname === "/sign-up/success";
+
   return (
     <header className={styles.siteHeader}>
       <Logo />
       <nav className={styles.mainNav}>
         <Link href="/">Home</Link>
-        <Link href="/sign-up">
-          <Button variant="contained" color="primary">
-            Sign Up
-          </Button>
-        </Link>
+        {successScreenVisible ? (
+          <Link href="/">Log Out</Link>
+        ) : (
+          <Link href="/sign-up">
+            <Button variant="contained" color="primary">
+              Sign Up
+            </Button>
+          </Link>
+        )}
       </nav>
     </header>
   );
